3dd30f5ff4d0859250ecd7aacdc04d25b6a917ca
erik santiago <erikillzon@gmail.com>
2023-04-18 14:57:10 -0500

    commit inicial

---
+++ src/main/java/chatu/ChatucorpApplication.java
@@ -0,0 +1,13 @@
+package chatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatucorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatucorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chatu/ChatucorpApplicationTests.java
@@ -0,0 +1,13 @@
+package chatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatucorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
